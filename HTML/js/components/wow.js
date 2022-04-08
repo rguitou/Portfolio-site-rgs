2738f2476226d71f31b48804f630bbe578106830
rguitou <51365315+rguitou@users.noreply.github.com>
2022-04-08 14:10:34 +0200

    Add files via upload

---
+++ HTML/js/components/wow.js
@@ -0,0 +1,24 @@
+var Wow = function() {
+    "use strict";
+
+
+    var handleWow = function() {
+        var wow = new WOW({
+		    boxClass:     'wow',      
+		    offset:       0,          
+		    mobile:       false,      
+		    tablet:       false       
+		});
+		wow.init();
+    }
+
+    return {
+        init: function() {
+            handleWow(); 
+        }
+    }
+}();
+
+$(document).ready(function() {
+    Wow.init();
+});
